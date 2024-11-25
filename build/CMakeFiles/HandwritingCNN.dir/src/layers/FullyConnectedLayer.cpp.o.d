86d3e1845bf6ba14a6135af00b81b2ea970c71c4
Your Name <abhigyandoshi1@gmail.com>
2024-11-25 15:18:56 -0500

    Added Layer and Convolutional Layer files

--- build/CMakeFiles/HandwritingCNN.dir/src/layers/FullyConnectedLayer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HandwritingCNN.dir/src/layers/FullyConnectedLayer.cpp.o: \
- /home/abhigyan1290/projects/CNN-Handwriting/src/layers/FullyConnectedLayer.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/HandwritingCNN.dir/src/layers/PoolingLayer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HandwritingCNN.dir/src/layers/PoolingLayer.cpp.o: \
- /home/abhigyan1290/projects/CNN-Handwriting/src/layers/PoolingLayer.cpp \
- /usr/include/stdc-predef.h
